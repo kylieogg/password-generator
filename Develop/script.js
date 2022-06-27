14ed29be2c46a8e25e7bf8456e3b8908071ffe90
kylieogg <kylieogg1@gmail.com>
2022-06-27 10:03:26 -0400

    added functions to create random lists for password options

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,14 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
+
 // 1. Prompt the user for the password criteria.
 //  a. Password length 8 < 128.
 //  b. Lowercase, uppercase, numbers, special characters.
@@ -23,7 +31,7 @@ function generatePassword() {
   var userWantsNumbers = window.confirm("Would you like to include numeric characters in your password?")
   var userWantsCharacters = window.confirm("Would you like to include special characters in your password?")
   var userWantsLowercase = window.confirm("Would you like to include lowercase characters in your password?")
-  var userWantUppercase = window.confirm("Would you like to include uppercase characters in your password?")
+  var userWantsUppercase = window.confirm("Would you like to include uppercase characters in your password?")
 
   // arrays to include password possibilities
   var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -31,10 +39,38 @@ function generatePassword() {
   var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", " Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-  
+
 // 2. Validate the input.
 // 3. Generate password based on criteria.
 
+  var passwordOptions = []
+
+  if (userWantsNumbers === true) {
+    passwordOptions.push(numberList)
+  }
+
+  if (userWantsCharacters === true) {
+    passwordOptions.push(characterList)
+  }
+
+  if (userWantsLowercase === true) {
+    passwordOptions.push(lowercaseList)
+  }
+
+  if (userWantsUppercase === true) {
+    passwordOptions.push(uppercaseList)
+  }
+
+
+  var generatedPassword = ""
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomList = getRandom(passwordOptions)
+    var randomChar = getRandom(randomList)
+
+    console.log(randomChar)
+  }
+    
 
 // 4. Display password to the page.
   return "Generated password will go here!";
