cc9858bbf4429dcf37a840091e6b5b15e3927a44
kylieogg <kylieogg1@gmail.com>
2022-06-26 21:11:32 -0400

    added user prompts and created array of possible password characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,37 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  console.log("Hey! You clicked the button!")
-
 // 1. Prompt the user for the password criteria.
 //  a. Password length 8 < 128.
 //  b. Lowercase, uppercase, numbers, special characters.
+function generatePassword() {
+  
+  var userInput = window.prompt("How many characters would you like your password to contain?")
+
+  var passwordLength = parseInt(userInput)
+
+  if (isNaN(passwordLength)) {
+    window.alert("That is not a number. Please enter a number.")
+    return
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be between 8 and 128 characters.")
+    return
+  }
+
+  var userWantsNumbers = window.confirm("Would you like to include numeric characters in your password?")
+  var userWantsCharacters = window.confirm("Would you like to include special characters in your password?")
+  var userWantsLowercase = window.confirm("Would you like to include lowercase characters in your password?")
+  var userWantUppercase = window.confirm("Would you like to include uppercase characters in your password?")
+
+  // arrays to include password possibilities
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var characterList = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", " Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+  
 // 2. Validate the input.
 // 3. Generate password based on criteria.
 
